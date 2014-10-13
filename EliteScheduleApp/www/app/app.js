6a244e2801bac942cb5ba82e6427f974c4d7ae5a
Michael Bowlin <info@michaelbowlin.com>
2014-10-13 11:07:28 -0600

    Jumped ahead in tut w/out documenting in the ReadMe. Checked everything against the tutorial source.

--- EliteScheduleApp/www/app/app.js
+++ EliteScheduleApp/www/app/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('eliteApp', ['ionic'])
+angular.module("eliteApp", ["ionic"])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,21 +22,93 @@ angular.module('eliteApp', ['ionic'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
+
   $stateProvider
 
     .state('home', {
+      abstract: true,
       url: "/home",
-      //abstract: true,
-      templateUrl: "app/home/home.html",
-      //controller: 'AppCtrl'
+      templateUrl: "app/home/home.html"
+    })
+
+    .state('home.leagues', {
+      url: "/leagues",
+      views: {
+        "tab-leagues": {
+          templateUrl: "app/home/leagues.html"
+        }
+      }
+    })
+
+    .state('home.myteams', {
+      url: "/myteams",
+      views: {
+        "tab-myteams": {
+          templateUrl: "app/home/myteams.html"
+        }
+      }
     })
 
     .state('app', {
+      abstract: true,
       url: "/app",
       templateUrl: "app/layout/menu-layout.html"
+    })
+
+    .state('app.teams', {
+      url: "/teams",
+      views: {
+        'mainContent': {
+          templateUrl: "app/teams/teams.html"
+        }
+      }
+    })
+
+    .state('app.team-detail', {
+      url: "/teams/:id",
+      views: {
+        'mainContent': {
+          templateUrl: "app/teams/team-detail.html"
+        }
+      }
+    })
+
+    .state('app.game', {
+      url: "/game/:id",
+      views: {
+        'mainContent': {
+          templateUrl: "app/game/game.html"
+        }
+      }
+    })
+
+    .state('app.standings', {
+      url: "/standings",
+      views: {
+        'mainContent': {
+          templateUrl: "app/standings/standings.html"
+        }
+      }
+    })
+
+    .state('app.locations', {
+      url: "/locations",
+      views: {
+        'mainContent': {
+          templateUrl: "app/locations/locations.html"
+        }
+      }
+    })
+
+    .state('app.rules', {
+      url: "/rules",
+      views: {
+        'mainContent': {
+          templateUrl: "app/rules/rules.html",
+        }
+      }
     });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app');
+    // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/teams');
 });
-
